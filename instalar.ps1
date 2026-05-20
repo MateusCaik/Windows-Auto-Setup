21bdff563206677719548a1ba0eceaf50f17cce0
MateusCaik <142317952+MateusCaik@users.noreply.github.com>
2026-05-20 10:09:20 -0300

    Add files via upload

---
+++ instalar.ps1
@@ -0,0 +1,20 @@
+# Atualizar winget
+winget source reset --force
+winget source update
+
+winget upgrade Microsoft.AppInstaller `
+  --source winget `
+  --silent `
+  --accept-package-agreements `
+  --accept-source-agreements
+
+# Instalar programas
+winget install Google.Chrome --source winget --silent --accept-package-agreements --accept-source-agreements
+
+winget install Mozilla.Firefox --source winget --silent --accept-package-agreements --accept-source-agreements
+
+winget install Oracle.JavaRuntimeEnvironment --source winget --silent --accept-package-agreements --accept-source-agreements
+
+winget install RARLab.WinRAR --source winget --silent --accept-package-agreements --accept-source-agreements
+
+winget install Microsoft.Office --source winget --silent --accept-package-agreements --accept-source-agreements
