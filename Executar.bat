ce9da4b024426f91e63970f5f5b8c39e0f383415
MateusCaik <142317952+MateusCaik@users.noreply.github.com>
2026-05-20 12:54:59 -0300

    Update Executar.bat

--- Executar.bat
+++ Executar.bat
@@ -1,6 +1,6 @@
 @echo off
 cd /d "%~dp0"
 
-powershell -ExecutionPolicy Bypass -NoProfile -File "instalar.ps1"
+powershell -ExecutionPolicy Bypass -NoProfile -File "%~dp0instalar.ps1"
 
-pause
+exit
